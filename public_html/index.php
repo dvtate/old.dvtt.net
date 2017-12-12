46a62958995fbdea6cfb513193e3e346f0757682
Dustin Van Tate Testa <toast27@gmail.com>
2017-12-12 09:10:50 -0500

    minor changes and updates

--- public_html/index.php
+++ public_html/index.php
@@ -112,66 +112,71 @@
 				<span class="lgfull" id="col1row1lg" onclick="resetboxes()">
 					<h2 title="double-click to see how busy I've been and what has changed since last time"><b>Updates</b></h2>
 					<ul id="update-timeline">
-						<li><b>2017.11.15</b>: move website to pi server after web host begins misbehaving</li>
-						<li><b>2017.9.5</b>: convert pi server into a shadowsocks server so that my robotics team and I can use the real internet at school.</li>
-						<li><b>2017.8.25</b>: School blocks expressvpn, which I was paying $200/year for (2 subs, shared acct.)</li>
-						<li><b>2017.8.7</b>: began learing Chinese on GA Virtual School so I can communicate with internet friends.</li>
-						<li><b>2017.7.6</b>: made a raspberry pi home server running Arch Linux ARM. (for robotics chat bot, discord bot, and shadowsocks server)</li>
-						<li><b>2017.6.23</b>: began developing a <a href="https://github.com/robobibb/robobibb-steve-bot/" target="_blank">Telegram bot</a> (node.js) for my robotics team</li>
-						<li><b>2017.6.11</b>: registered cloudflare account, <a href="https://dvtate.com">https</a> enabled.</li>
-						<li><b>2017.6.5</b>: hosting provider changed service, minor changes</li>
-						<li><b>2017.3.8</b>: started working on <a href="https://robobibb.github.io" target="_blank">robotics team's website</a> with my programming team.</li>
-						<li><b>2016.10.2</b>: made a <a href="https://github.com/dvtate/dvtate.com" target="_blank">GitHub repo for this website</a> so you can see my source.</li>
-						<li><b>2016.8.20</b>: got annoied by my website's obsoleteness. Spiced things up with some Serial Experiments lain themed content and some <span title="from when my parents made me get off my gfs vps">minor fixes</span>.</li>
-						<li><b>2016.8.1</b>: began working for school district as an IT intern</li>
-						<li><b>2016.8.1</b>: School starts. 5 AP Classes. This is gonna be difficult.</li>
-						<li><b>2016.6.19</b>: went to GHP for software (and electrical) engineering.</li>
-						<li><b>2016.6.5</b>: began developing my own scripting language - <a href="https://github.com/dvtate/yoda/" target="_blank">YodaScript</a>, while volunteering at a summer camp.</li>
-						<li><b>2016.4.4</b>: parents threw my workstation out of window because they didn't like my GF... (qwq)</li>
-						<li><b>2016.2.14</b>: parents decieded I should take a break from the computer.</li>
-						<li><b>2016.2.13</b>: Went public with my relationship to my GF.</li>
-						<li><b>2016.1.1</b>: Got addicted to C/C++/Arduino and put the website on the back-burner.</li>
-						<li><b>2015.12.4</b>: Moved website to my girlfriend's server.</li>
-						<li><b>2015.11.20</b>: 16<sup>th</sup> birthday.</li>
-						<li><b>2015.9.2</b>: Started work on a <a href="//dvtate.com/healthit/">project for HealthIT</a>.</li>
-						<li><b>2015.8.5</b>: Rebuilt homepage with new style (<a href="//dvtate.com/trusty.html">view old</a>).</li>
-						<li><b>2015.8.3</b>: Began learning Spanish at school.</li>
-						<li><b>2015.7.27</b>: Built the <a href="//dvtate.com/os/">operating systems</a> page.</li>
-						<li><b>2015.6.11</b>: Built <a href="//dvtate.com/3d/">3D modeling</a> page.</li>
-						<li><b>2015.6.5</b>: Added a <a href="//dvtate.com/school/">college</a> page. But there's a trick :)</li>
-						<li><b>2015.5.21</b>: converted all <a href="//dvtate.com/calc/">calculators</a> to JavaScript.</li>
-						<li><b>2015.5.12</b>: Added mobile support for iOS.</li>
-						<li><b>2015.5.9</b>: Built <a href="//dvtate.com/forge/">forge</a> page. Adjustments to <a href="//dvtate.com/chat/">chatroom</a></li>
-						<li><b>2015.5.5</b>: rebuilt <a href="//dvtate.com/prog/">programming</a> page to fit new style</li>
-						<li><b>2015.4.20</b>: registered new domain (<a href="//dvtate.com/">dvtate.com</a>) to replace <a class="broken-link" href="//dvtate.com/pageremoved.html" title="I no longer own this url (broken-link)">dvtate.hostoi.com</a></li>
-						<li><b>2015.4.12</b>: Finished <a href="http://dvtate.com/chat/">chatroom</a>.</li>
-						<li><b>2015.4.6</b>: Began Work on <a href="//dvtate.com/chat/">chatroom</a>, Favorite language changed to <a href="http://www.dvtate.com/prog/php/">PHP</a></li>
-						<li><b>2015.2.16</b>: Installed <a href="https://www.archlinux.org/" target="_blank">Arch Linux</a> for the first time.</li>
-						<li><b>2015.2.13</b>: Adjustments to page menu, fonts, style, drop-down menus, etc.(<a href="http://dvtate.com/draft.html">view old</a>)</li>
-						<li><b>2015.2.2</b>: Rebuilt website homepage from scratch (<a href="//dvtate.com/olddefault.html">view old</a>).</li>
-						<li><b>2015.1.20</b>: Made a summations calculator, and a resistor color code calculator.</li>
-						<li><b>2015.1.15</b>: Started playing with <a href="http://www.erlang.org/" target="_blank">Erlang</a>.</li>
-						<li><b>2014.11.20</b>: My 15<sup>th</sup> birthday.</li>
-						<li><b>2014.9.19</b>: Built original programming page</li>
-						<li><b>2014.9.5</b>: Rebuilt <a  href="//dvtate.com/prog/php/tri.php" >tri.php</a> to fit homepage style</li>
-						<li><b>2014.8.27</b>: Built original homepage.</li>
-						<li><b>2014.7.1</b>: Started playing with C# (within <a href="http://www.unity3d.com/" target="_blank">Unity</a>)</li>
-						<li><b>2014.6.30</b>: converted tri.asp to <a href="//dvtate.com/prog/php/oldtri.php">oldtri.php</a></li>
-						<li><b>2013.1</b>: built first forge</li>
-						<li><b>2012.11</b>: began learning YA-BASIC as first programming language.</li>
-						<li><b>2012.10</b>: made earthquake machine 3000</li>
-						<li><b>2012.4</b>: first arduino (uno)</li>
-						<li><b>2011.10</b>: made earthquake machine 2000</li>
-						<li><b>2010</b>: began learning Danish online in freetime.</li>
+						<li><b>2017.12.11</b>:	began work on <a href="https://dvtate.github.io" target="_blank">my github page</a> as a place to experiment with web design.</li>
+						<li><b>2017.11.15</b>:	move website to pi server after web host begins misbehaving</li>
+						<li><b>2017.9.5</b>:	convert pi server into a shadowsocks server so that my robotics team and I can use the real internet at school.</li>
+						<li><b>2017.8.25</b>:	School blocks expressvpn, which I was paying $200/year for (2 subs, shared acct.)</li>
+						<li><b>2017.8.7</b>:	began learing Chinese on GA Virtual School so I can communicate with internet friends.</li>
+						<li><b>2017.7.6</b>:	made a raspberry pi home server running Arch Linux ARM. (for robotics chat bot, discord bot, and shadowsocks server)</li>
+						<li><b>2017.6.23</b>:	began developing a <a href="https://github.com/robobibb/robobibb-steve-bot/" target="_blank">Telegram bot</a> (node.js) for my robotics team</li>
+						<li><b>2017.6.11</b>:	registered cloudflare account, <a href="https://dvtate.com">https</a> enabled.</li>
+						<li><b>2017.6.5</b>:	hosting provider changed service, minor changes</li>
+						<li><b>2017.3.8</b>:	started working on <a href="https://robobibb.github.io" target="_blank">robotics team's website</a> with my programming team.</li>
+						<li><b>2016.10.2</b>:	made a <a href="https://github.com/dvtate/dvtate.com" target="_blank">GitHub repo for this website</a> so you can see my source.</li>
+						<li><b>2016.8.20</b>:	got annoied by my website's obsoleteness. Spiced things up with some Serial Experiments lain themed content and some <span title="from when my parents made me get off my gfs vps">minor fixes</span>.</li>
+						<li><b>2016.8.1</b>:	School starts. 5 AP Classes. This is gonna be difficult.</li>
+						<li><b>2016.6.19</b>:	went to GHP for software (and electrical) engineering.</li>
+						<li><b>2016.6.5</b>:	began developing my own scripting language - <a href="https://github.com/dvtate/yoda/" target="_blank">YodaScript</a>, while volunteering at a summer camp.</li>
+						<li><b>2016.4.4</b>:	parents threw my workstation out of window because they didn't like my GF... (qwq)</li>
+						<li><b>2016.2.13</b>:	Went public with my relationship to my GF.</li>
+						<li><b>2016.1.1</b>:	Got addicted to C/C++/Arduino and put the website on the back-burner.</li>
+						<li><b>2015.12.4</b>:	Moved website to my girlfriend's server.</li>
+						<li><b>2015.11.20</b>:	16<sup>th</sup> birthday.</li>
+						<li><b>2015.9.2</b>:	Started work on a <a href="//dvtate.com/healthit/">project for HealthIT</a>.</li>
+						<li><b>2015.8.5</b>:	Rebuilt homepage with new style (<a href="//dvtate.com/trusty.html">view old</a>).</li>
+						<li><b>2015.8.3</b>:	Began learning Spanish at school.</li>
+						<li><b>2015.7.27</b>:	Built the <a href="//dvtate.com/os/">operating systems</a> page.</li>
+						<li><b>2015.6.11</b>:	Built <a href="//dvtate.com/3d/">3D modeling</a> page.</li>
+						<li><b>2015.6.5</b>:	Added a <a href="//dvtate.com/school/">college</a> page. But there's a trick :)</li>
+						<li><b>2015.5.21</b>:	converted all <a href="//dvtate.com/calc/">calculators</a> to JavaScript.</li>
+						<li><b>2015.5.12</b>:	Added mobile support for iOS.</li>
+						<li><b>2015.5.9</b>:	Built <a href="//dvtate.com/forge/">forge</a> page. Adjustments to <a href="//dvtate.com/chat/">chatroom</a></li>
+						<li><b>2015.5.5</b>:	rebuilt <a href="//dvtate.com/prog/">programming</a> page to fit new style</li>
+						<li><b>2015.4.20</b>:	registered new domain (<a href="//dvtate.com/">dvtate.com</a>) to replace <a class="broken-link" href="//dvtate.com/pageremoved.html" title="I no longer own this url (broken-link)">dvtate.hostoi.com</a></li>
+						<li><b>2015.4.12</b>:	Finished <a href="http://dvtate.com/chat/">chatroom</a>.</li>
+						<li><b>2015.4.6</b>:	Began Work on <a href="//dvtate.com/chat/">chatroom</a>, Favorite language changed to <a href="http://www.dvtate.com/prog/php/">PHP</a></li>
+						<li><b>2015.2.16</b>:	Installed <a href="https://www.archlinux.org/" target="_blank">Arch Linux</a> for the first time.</li>
+						<li><b>2015.2.13</b>:	Adjustments to page menu, fonts, style, drop-down menus, etc.(<a href="http://dvtate.com/draft.html">view old</a>)</li>
+						<li><b>2015.2.2</b>:	Rebuilt website homepage from scratch (<a href="//dvtate.com/olddefault.html">view old</a>).</li>
+						<li><b>2015.1.20</b>:	Made a summations calculator, and a resistor color code calculator.</li>
+						<li><b>2015.1.15</b>:	Started playing with <a href="http://www.erlang.org/" target="_blank">Erlang</a>.</li>
+						<li><b>2014.11.20</b>:	My 15<sup>th</sup> birthday.</li>
+						<li><b>2014.9.19</b>:	Built original programming page</li>
+						<li><b>2014.9.5</b>:	Rebuilt <a  href="//dvtate.com/prog/php/tri.php" >tri.php</a> to fit homepage style</li>
+						<li><b>2014.8.27</b>:	Built original homepage.</li>
+						<li><b>2014.7.1</b>:	Started playing with C# (within <a href="http://www.unity3d.com/" target="_blank">Unity</a>)</li>
+						<li><b>2014.6.30</b>:	converted tri.asp to <a href="//dvtate.com/prog/php/oldtri.php">oldtri.php</a></li>
+						<li><b>2013.1</b>:		built first forge</li>
+						<li><b>2012.11</b>:		began learning YA-BASIC as first programming language.</li>
+						<li><b>2012.10</b>:		made earthquake machine 3000</li>
+						<li><b>2012.4</b>:		first arduino (uno)</li>
+						<li><b>2011.10</b>:		made earthquake machine 2000</li>
+						<li><b>2010</b>:		began learning Danish online in freetime.</li>
 					</ul>
 				</span>
 			</td><td class="nybox" id="col2row1" onclick="focusbox(2,1)" title="double-click to See what's happening in the ChatRoom">
 				<span id="col2row1sm" style="display: none">
-					<h1>Chat<sup><span id="bubblealert"><?php echo getPostCount(); ?></span></sup></h1>
+					<h1>Chat<sup>
+						<span id="bubblealert"><?php
+							echo getPostCount();
+						?></span>
+					</sup></h1>
 				</span>
 				<span id="col2row1md">
 					<h2>Chat Room</h2><hr/>
-					<h4><span id="bubblealert"><?php echo getPostCount(); ?></span> messages</h4>
+					<h4><span id="bubblealert"><?php
+						echo getPostCount();
+					?></span> messages</h4>
 				</span>
 				<span class="lgfull" id="col2row1lg" onclick="resetboxes()">
 					<h2><b>Latest chatroom post</b>:</h2>
