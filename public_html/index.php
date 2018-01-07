4b01b1317713955bdf2d951e7d5a956aaf70337c
Dustin Van Tate Testa <toast27@gmail.com>
2018-01-07 18:49:57 -0500

    rip net neutrality

--- public_html/index.php
+++ public_html/index.php
@@ -243,7 +243,5 @@
 		<center><span id="social_footer"></span></center>
 		<script>genFooter("social_footer");</script>
 
-		<script src="https://widget.battleforthenet.com/widget.js" async></script>
-
 	</body>
 </html>
